24c270b5d51187f4e76824cf41c8187732499db5
Joao Paulo <jp.silva.info@gmail.com>
2026-05-26 23:37:57 -0300

    fix: corrige imports css do redesign

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import "./styles/theme.css";
+import "./styles/animations.css";
+import "./styles/layout.css";
 import "./styles/sidebar.css";
-import "./styles/kpi.css";
-import "./styles/responsive.css";
+import "./styles/cards.css";
+import "./styles/buttons.css";
+import "./styles/tables.css";
+import "./styles/mobile.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
