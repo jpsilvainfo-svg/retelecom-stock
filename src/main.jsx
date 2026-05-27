d9fe788fd2d9f4adc6dbb98494c3ead236bad954
Joao Paulo <jp.silva.info@gmail.com>
2026-05-26 23:26:19 -0300

    feat: novo visual premium stocktel

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/global.css";
+
+import "./styles/theme.css";
+import "./styles/sidebar.css";
+import "./styles/kpi.css";
+import "./styles/responsive.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
